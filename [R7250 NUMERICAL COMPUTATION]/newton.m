bf2c2f06d2ac85c754336772cb2ef7b534c9078f
Xuan Zhong <xuan.zhong06@gmail.com>
2022-03-17 20:27:14 -0700

    newton method

--- [R7250 NUMERICAL COMPUTATION]/newton.m
+++ [R7250 NUMERICAL COMPUTATION]/newton.m
@@ -1,11 +1,11 @@
 
 x0 = 100;
-y_target = 0;
+y_target = 40;
 
 x = x0;
 y = f(x);
 while abs(y - y_target) > eps
-    x = x - y/dfdx(x);
+    x = x - (y - y_target)/dfdx(x);
     y = f(x);
     disp(x)
 end
@@ -15,7 +15,7 @@ disp(x)
 
 
 function y_out = f(x_in)
-    y_out = x_in^2 - 81;
+    y_out = x_in^2 - 9;
 end
 
 
