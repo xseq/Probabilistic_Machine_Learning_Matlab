870cbb2686cf53bc17f2f80ebc00d1027d6eea33
Xuan Zhong <xuan.zhong06@gmail.com>
2022-03-20 20:14:15 -0700

    matrix multiplication

--- R7220_linear_algebra/L2_norm.m
+++ R7220_linear_algebra/L2_norm.m
@@ -3,7 +3,7 @@
 function norm_out = L2_norm(vector_in)
     summ = 0;
     for p = 1 : length(vector_in)
-        summ = summ + vector_in(p, 1)^2;
+        summ = summ + vector_in(1, p)^2;
     end
     norm_out = sqrt(summ);
 end
---
+++ R7220_linear_algebra/multiplication.m
@@ -0,0 +1,6 @@
+% y = A * x
+
+function out = multiplication(matraix_in, vector_in)
+    out = matraix_in * vector_in;
+end
+
