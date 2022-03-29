6a14767806a7d1b7b19726e3a112d83f01241849
Xuan Zhong <xuan.zhong06@gmail.com>
2022-03-28 21:42:02 -0700

    linear regression, SGD

---
+++ R7610_linear_regression/sgd_lms_solve.m
@@ -0,0 +1,43 @@
+% Murphy PML book section 8.4
+% Also see: 7610 notes and supplement materials
+
+% Dataset, first columns are X, last columne is Y
+clear; clc; close all;
+data_raw = [
+    4,1,2;
+    2,8,-14;
+    1,0,1;
+    3,2,-1;
+    1,4,-7;
+    6,7,-8
+    ];
+
+% Initialization
+epochs = 3;
+[rows, cols] = size(data_raw);
+n_samples = rows;
+n_weights = cols - 1;
+
+W = ones(1, n_weights);    % weights
+b = 0;
+L = zeros(n_samples * epochs, 1);    % loss
+eta = 0.05;                 % step size
+
+for p = 1 : epochs
+    for q = 1 : n_samples
+        X = data_raw(1, 1:cols-1);
+        y = data_raw(1, cols);
+
+        % Forward propogation
+        y_hat = X * W' + b;
+        err = y_hat - y;
+        L((p-1)*n_samples+q, 1) = err^2;
+
+        % Backward propogation
+        W = W - eta * err * X;
+        b = b - eta * err;    
+    end
+end
+
+plot(log(L))
+
