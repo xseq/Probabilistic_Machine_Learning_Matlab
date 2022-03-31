f9b2a64c18f9b3fca60798960d344ce4e100982c
Xuan Zhong <xuan.zhong06@gmail.com>
2022-03-30 23:13:20 -0700

    SGD online, added mlr07 data

--- R7610_linear_regression/sgd_lms_test.m
+++ R7610_linear_regression/sgd_lms_test.m
@@ -3,18 +3,19 @@
 
 % Dataset, first columns are X, last columne is Y
 clear; clc; close all;
-data_raw = [
-    4,1,2;
-    2,8,-14;
-    1,0,1;
-    3,2,-1;
-    1,4,-7;
-    6,7,-8
-    ];
+% data_raw = [
+%     4,1,2;
+%     2,8,-14;
+%     1,0,1;
+%     3,2,-1;
+%     1,4,-7;
+%     6,7,-8
+%     ];
+load('mlr_data_raw.mat');
 
-X_data = data_raw(:, 1:2);
-y_data = data_raw(:, 3);
-[model, L] = sgd_lms_solve(X_data, y_data, 0.001, 3000);
+X_data = mlr_data_raw(:, 2:end);
+y_data = mlr_data_raw(:, 1);
+[model, L] = sgd_lms_solve(X_data, y_data, 0.000001, 300);
 disp(model.W)
 disp(model.b)
 plot(log(L))
