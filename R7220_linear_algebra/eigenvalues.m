ecfa137cb2900219a6a2c0b3f435745a8ec550c5
Xuan Zhong <xuan.zhong06@gmail.com>
2022-04-01 16:11:59 -0700

    eigenvalues

--- R7220_linear_algebra/eigenvalues.m
+++ R7220_linear_algebra/eigenvalues.m
@@ -1,13 +1,13 @@
 % Murphy PML 7.4
 
 clear; clc;
-X = [1, 0; 
-    1, 0];
+X = [3, 4; 
+    5, 6];
 [V, D] = eig(X)
 v1 = V(:, 1)
 v2 = V(:, 2)
-aaa = X * v1
-bbb = D(1, 1) .* v1
+aaa1 = X * v1
+aaa2 = D(1, 1) .* v1
 
 
 
