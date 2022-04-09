5494d9dd31f6dd1e8d98ee8b45fae711070a60b6
Xuan Zhong <xuan.zhong06@gmail.com>
2022-04-08 18:16:58 -0700

    linear algebra basics

--- R7220_linear_algebra/linear_algebra_basics.m
+++ R7220_linear_algebra/linear_algebra_basics.m
@@ -3,9 +3,14 @@ a_matrix =  [0, 1;
         1, 0]
 a_rank = rank(a_matrix)
 a_reshaped = reshape(a_matrix, [2, 3])
+a_condition = cond(a_matrix)
+
 c_matrix = a_matrix*a_matrix'
+c_rank = rank(c_matrix)
 c_trace = trace(c)
 c_determinant = det(c)
+c_orthogonal = orth(c)
+
 [u_matrix, s_matrix, v_matrix] = svd(a_matrix)
 
 
