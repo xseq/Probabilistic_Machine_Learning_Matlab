569c355cbb5a5391f37b10b601711d1effdeaa10
Xuan Zhong <xuan.zhong06@gmail.com>
2022-04-10 16:38:56 -0700

    image pca

--- R7220_linear_algebra/image_pca.m
+++ R7220_linear_algebra/image_pca.m
@@ -3,11 +3,18 @@
 
 % get the gray image data
 img = imread('cityscape.jpg');
-img_gray = rgb2gray(img);
+img_gray = im2double(rgb2gray(img));
 
 % take only the square part
 [original_row, origina_col] = size(img_gray);
 len_edge = min(original_row, origina_col);
 data = img_gray(1:len_edge, 1:len_edge);
 % imshow(data);
+
+% eigenvalues and eigenvectors
+eig_values = eig(data);
+[V, D] = eig(data);
+out = V * D * V';
+imshow(out);
+
 
