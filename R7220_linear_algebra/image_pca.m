bc72ae2f98d3460befed673f388d0814b4315739
Xuan Zhong <xuan.zhong06@gmail.com>
2022-04-12 22:40:02 -0700

    minor changes

--- R7220_linear_algebra/image_pca.m
+++ R7220_linear_algebra/image_pca.m
@@ -6,41 +6,34 @@ clear;
 clc; 
 close all;
 
-
 % parameters
 comp_ratio = 0.3;
-
 
 % get the gray image data
 img = imread('cityscape.jpg');
 data = im2double(rgb2gray(img));
 [n_col, ~] = size(img);
-
+% TODO optional: block processing
 
 % whitening
 avg = mean(mean(data));
 white_data = data - avg;
 
-
 % covariance matrix
 cov_mat = white_data * white_data';
-
 
 % eigen values and vectors
 [eig_vctr, eig_values] = eig(cov_mat);
 [~, order] = sort(diag(eig_values), 'descend');
 eig_vctr = eig_vctr(:, order);
 
-
 % compression
 eig_vctr_comp = eig_vctr(:, 1:floor(n_col*comp_ratio));
 img_regen = (white_data' * eig_vctr_comp) * eig_vctr_comp';
 img_regen = img_regen' + avg;
-% img_diff = data - img_regen;
-
+% optional TODO: difference between original and compressed images.
 
 % plotting
 figure(1),subplot(121),imshow(data,[]); title('Original Image');
-figure(1),subplot(122),imshow(img_regen,[]); title('Recovered img');
-
+figure(1),subplot(122),imshow(img_regen,[]); title('Compressed Image');
 
