85055cc8fab1b0c70003fda6910624319b33f877
Xuan Zhong <xuan.zhong06@gmail.com>
2022-04-22 17:00:12 -0700

    Start working on LDA

--- [R7590 LDA]/LDA.m
+++ [R7590 LDA]/LDA.m
@@ -2,4 +2,15 @@
 % reference: https://github.com/farnazgh/Linear-discriminant-analysis
 
 clear; clc; close all;
+
+% load data
 load('7590_dataset.mat')
+
+% training data in variable train (shape 15216 x 10)
+% training label in variable trainLabels (shape 15216 x 1)
+% test data in variable test (shape 3480 x 10)
+% test label in variable testLabels (shape 3480 x 1)
+
+
+
+
