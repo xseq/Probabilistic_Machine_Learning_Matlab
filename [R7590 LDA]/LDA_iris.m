95fc4be79db264f693e0361b0c0f88a7a8fd2dd4
Xuan Zhong <xuan.zhong06@gmail.com>
2022-04-24 13:51:19 -0700

    LDA dataset

---
+++ [R7590 LDA]/LDA_iris.m
@@ -0,0 +1,5 @@
+% running linear discriminant analysis on iris dataset
+% reference: https://www.adeveloperdiary.com/data-science/machine-learning/linear-discriminant-analysis-from-theory-to-code/
+% https://zhuanlan.zhihu.com/p/264578345
+
+load('./data/iris.data')
