7bdea2c7ec6850507840fed005b361a9c619f319
Xuan Zhong <xuan.zhong06@gmail.com>
2022-05-15 18:13:28 -0700

    mvn

---
+++ CHPT 3 MULTIVARIATE MODELS/get_mvn.m
@@ -0,0 +1,14 @@
+% Get multivariate normal (Gaussian) distribution
+% This script relates to Probabilistic Machine Learning by Murphy
+% eq. 3.11
+
+
+function prob_out = get_mvn(data_in, avg_in, cov_in)
+    % todo: error reporting for wrong input matrix dimensions
+    D = length(cov_in);
+    cov_det = det(cov_in);
+    y_center = data_in - avg_in;
+    inv_cov = 1 / cov_in;
+    prob_out = 1 / ((2 * pi)^(D / 2) * sqrt(cov_det)) * ...
+        exp(-1/2 * y_center' * inv_cov * y_center);
+end
