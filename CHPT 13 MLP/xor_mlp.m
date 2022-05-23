f49fcff3db66cbc57c0bbbcd1cfa28bd0b63e52c
Xuan Zhong <xuan.zhong06@gmail.com>
2022-05-22 17:27:42 -0700

    fixing BP

--- CHPT 13 MLP/xor_mlp.m
+++ CHPT 13 MLP/xor_mlp.m
@@ -4,6 +4,7 @@
 % notations: https://zhuanlan.zhihu.com/p/40761721
 % back propogation reference: https://zerowithdot.com/mlp-backpropagation/
 % BP video: https://www.youtube.com/watch?v=tIeHLnjs5U8
+% Best explanation with code: https://brilliant.org/wiki/backpropagation/
 
 
 clear all; clc; close all;
@@ -22,7 +23,7 @@ y = [0, 1, 1, 0];
 XX = [X, ones(n_samples, 1)]';
 
 % parameters
-eta = 0.7;
+eta = 0.01;
 epochs = 10000;
 W2 = ones(3, 2);
 W3 = ones(3, 1);
@@ -42,51 +43,56 @@ for p = 1 : epochs
     Z3 = W3' * a2e;     % 1x4
     a3 = sigmoid(Z3);   % 1x4
     
-    % loss: residual sum of squares, eq. 11.6
-    DF3 = a3 .* (1 - a3) .* (y - a3);   % error
+    % loss
+    loss = sum((a3 - y).^2) / n_samples;
+    loss_rec(p, 1) = loss;
+    
+    % partial derivative: residual sum of squares, eq. 11.6
+    DF3 = a3 .* (1 - a3) .* (y - a3); 
     
     % back propagation
-    DF2 = W3' * DF3; % .* Z3 .* (1 - Z3));
+    DF2 = Z3 .* (1 - Z3) .* (W3(1:2, :) * DF3);
     
     % weight update
-    W3 = W3 + eta * H2_input' * DF3;
-    W2 = W2 + eta * XX' * DF2;
+    W3 = W3 + eta * a2e * DF3';
+    W2 = W2 + eta * XX * DF2';
     
 end
 
+plot(loss_rec);
 
 
 
-for i = 1:iterations
-   out = zeros(4,1);
-   numIn = length (input(:,1));
-   for j = 1:numIn
-
-      % Adjust delta values of weights
-      % For output layer:
-      % delta(wi) = xi*delta,
-      % delta = (1-actual output)*(desired output - actual output) 
-      delta3_1 = out(j)*(1-out(j))*(output(j)-out(j));
-      
-      % Propagate the delta backwards into hidden layers
-      delta2_1 = x2(1)*(1-x2(1))*weights(3,2)*delta3_1;
-      delta2_2 = x2(2)*(1-x2(2))*weights(3,3)*delta3_1;
-      
-      % Add weight changes to original weights 
-      % And use the new weights to repeat process.
-      % delta weight = coeff*x*delta
-      for k = 1:3
-         if k == 1 % Bias cases
-            weights(1,k) = weights(1,k) + coeff*bias(1,1)*delta2_1;
-            weights(2,k) = weights(2,k) + coeff*bias(1,2)*delta2_2;
-            weights(3,k) = weights(3,k) + coeff*bias(1,3)*delta3_1;
-         else % When k=2 or 3 input cases to neurons
-            weights(1,k) = weights(1,k) + coeff*input(j,1)*delta2_1;
-            weights(2,k) = weights(2,k) + coeff*input(j,2)*delta2_2;
-            weights(3,k) = weights(3,k) + coeff*x2(k-1)*delta3_1;
-         end
-      end
-   end   
-end
-
+% for i = 1:iterations
+%    out = zeros(4,1);
+%    numIn = length (input(:,1));
+%    for j = 1:numIn
+% 
+%       % Adjust delta values of weights
+%       % For output layer:
+%       % delta(wi) = xi*delta,
+%       % delta = (1-actual output)*(desired output - actual output) 
+%       delta3_1 = out(j)*(1-out(j))*(output(j)-out(j));
+%       
+%       % Propagate the delta backwards into hidden layers
+%       delta2_1 = x2(1)*(1-x2(1))*weights(3,2)*delta3_1;
+%       delta2_2 = x2(2)*(1-x2(2))*weights(3,3)*delta3_1;
+%       
+%       % Add weight changes to original weights 
+%       % And use the new weights to repeat process.
+%       % delta weight = coeff*x*delta
+%       for k = 1:3
+%          if k == 1 % Bias cases
+%             weights(1,k) = weights(1,k) + coeff*bias(1,1)*delta2_1;
+%             weights(2,k) = weights(2,k) + coeff*bias(1,2)*delta2_2;
+%             weights(3,k) = weights(3,k) + coeff*bias(1,3)*delta3_1;
+%          else % When k=2 or 3 input cases to neurons
+%             weights(1,k) = weights(1,k) + coeff*input(j,1)*delta2_1;
+%             weights(2,k) = weights(2,k) + coeff*input(j,2)*delta2_2;
+%             weights(3,k) = weights(3,k) + coeff*x2(k-1)*delta3_1;
+%          end
+%       end
+%    end   
+% end
+% 
 
