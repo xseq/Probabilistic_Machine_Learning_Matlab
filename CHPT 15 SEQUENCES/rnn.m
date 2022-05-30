9bf3bbca70ccd89344d67e357b71c793388df98e
Xuan Zhong <xuan.zhong06@gmail.com>
2022-05-30 15:30:40 -0700

    rnn data loading

--- CHPT 15 SEQUENCES/rnn.m
+++ CHPT 15 SEQUENCES/rnn.m
@@ -7,3 +7,15 @@ clear; clc; close all;
 data = cell2mat(raw_data(2:end, 2));
 plot(data);
 
+
+% splitting data set
+data_ratio = 0.8;
+n_total_samples = length(data);
+n_train_samples = round(data_ratio * n_total_samples);
+n_test_samples = n_total_samples - n_train_samples;
+train_data = data(1 : n_train_samples);
+test_data = data(n_train_samples+1 : n_train_samples);
+
+
+
+
