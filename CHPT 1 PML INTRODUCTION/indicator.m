8f7a1a733927eacf254ae5432963a08bfb1b6d06
Xuan Zhong <xuan.zhong06@gmail.com>
2022-06-13 23:23:46 -0700

    chpt 2 adding distributions

---
+++ CHPT 1 PML INTRODUCTION/indicator.m
@@ -0,0 +1,8 @@
+% indicator function, the big "I", PML equation 1.3
+% this function is rather simple but its symbol is unique and would be used
+% a lot in the rest of the book
+
+
+function num_out = indicator(logical_in)
+    num_out = double(logical_in);
+end
